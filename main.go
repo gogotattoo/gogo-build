1a8f68c5b36f954c9b9d8b0bf0247047f76bceca
Roman Tronenko <roman@italki.com>
2017-04-01 17:39:54 +0800

    working

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
@@ -9,25 +9,25 @@ import (
 )
 
 func main() {
+	renderSite("gogo.tattoo", "http")
+	//renderSite("gogotattoo.github.io", "https")
+
+}
+
+func renderSite(out, protocol string) {
 	gopath := os.Getenv("GOPATH")
 	gogotattooPrefix := "/src/github.com/gogotattoo/"
-	artists := []string{"gogo", "aid", "xizi"}
+	artists := []string{"xizi"}
 	dir, err := filepath.Abs(filepath.Dir(os.Args[0]))
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(dir)
-	runHugo(gopath+gogotattooPrefix+"/gogo.tattoo", dir+"/gogo.tattoo/", "http://gogo.tattoo/")
+	renderMainSite(gopath+gogotattooPrefix+"/gogo.tattoo", dir+"/"+out, protocol+"://"+out+"/")
 	for _, artist := range artists {
-		runHugo(gopath+gogotattooPrefix+artist, dir+"/gogo.tattoo/"+artist, "http://gogo.tattoo/"+artist)
-	}
-	runHugo(gopath+gogotattooPrefix+"/gogotattoo.github.io", dir+"/gogotattoo.github.io/", "https://gogotattoo.github.io/")
-	for _, artist := range artists {
-		runHugo(gopath+gogotattooPrefix+artist, dir+"/gogotattoo.github.io/"+artist, "https://gogotattoo.github.io/"+artist)
+		renderArtistSite(gopath+gogotattooPrefix+artist, dir+"/"+out+"/"+artist, protocol+"://"+out+"/"+artist)
 	}
 }
-
-func runHugo(source, destination, baseURL string) {
+func renderMainSite(source, destination, baseURL string) {
 	cmd := exec.Command("hugo",
 		"--destination", destination,
 		"--source", source,
@@ -36,4 +36,37 @@ func runHugo(source, destination, baseURL string) {
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
 	cmd.Run()
+}
+
+var filesToDelete []string
+
+func addLocalizedFiles(path string, info os.FileInfo, err error) error {
+	newPath := path[:len(path)-3] + ".zh.md"
+	er := copyFile(path, newPath)
+	if er == nil {
+		filesToDelete = append(filesToDelete, newPath)
+	}
+	return nil
+}
+
+func renderArtistSite(source, destination, baseURL string) {
+	filepath.Walk(source+"/content/design", addLocalizedFiles)
+}
+func copyFile(dst, src string) error {
+	in, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer in.Close()
+	out, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer out.Close()
+	_, err = io.Copy(out, in)
+	cerr := out.Close()
+	if err != nil {
+		return err
+	}
+	return cerr
 }
